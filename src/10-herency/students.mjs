96baddb1495c82c9cc9a3a019cfd750650158972
Christian <c.espinoza.code@gmail.com>
2023-02-03 10:11:01 -0500

    aproveCourse method implemented

--- src/10-herency/students.mjs
+++ src/10-herency/students.mjs
@@ -34,5 +34,14 @@ export class FreeStudent extends Student {
   constructor(props) {
     super(props);
   }
-}
 
+  approveCourse(newCourse) {
+    if (newCourse.isFree) {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        `Lo sentimos, ${this.name}, solo puedes tomar cursos abiertos`
+      );
+    }
+  }
+}
